1a439d818f0939ad9afdd8a541fe67eeaf8a9b3a
tamer1645sk-max <tamer1645sk@gmail.com>
2026-06-04 17:27:04 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+print (" AI Bot Started ")
+while True:
+  user_input == input("You : ")
+
+if user_input == "Exit" :
+  print("Bot: Goodbye 👋")
+  break
+  
+   if"Hello" in user_input.lower():
+    print(" Bot : Hello! How can I help You ?")
+elif "Name" in user_input.lower():
+print(" Bot : I am Your AI assistant.")
+elif "How Are You" in user_input.lower():
+print(" Bot : I'm just code , but I'm  working fine! ")
+else:
+print(" Bot : I don't understand yet ")  
